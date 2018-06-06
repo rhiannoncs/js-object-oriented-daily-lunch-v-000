c9525604f15990324d731f6a9cec9faaf8229b32
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-06 15:32:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ class Meal {
   }
   
   static byPrice() {
-    return store.meals.sort(function(a, b) {return a.price - b.price});
+    return store.meals.sort(function(a, b) {return b.price - a.price});
   }
 }
 
