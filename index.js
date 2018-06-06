690f826f1bf5d6805342f1063d097850854fa293
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-06 15:08:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ class Customer {
     
     store.customers.push(this);
   }
+  
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.customerId === this.id;
+    });
+  }
 }
 
 class Meal {
