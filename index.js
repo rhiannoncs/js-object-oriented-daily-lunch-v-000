b513e9fbcd48eee4cd0dc5d3b277e17a59188309
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-06 15:02:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodId = 0;
 let customerId = 0;
+let mealId = 0;
 
 class Neighborhood {
   constructor(name) {
@@ -20,5 +21,15 @@ class Customer {
     this.neighborhoodId = neighborhoodId;
     
     store.customers.push(this);
+  }
+}
+
+class Meal {
+  constructor(title, price) {
+    this.id = ++mealId;
+    this.title = title;
+    this.price = price;
+    
+    store.meals.push(this);
   }
 }
