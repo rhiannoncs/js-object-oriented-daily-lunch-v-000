5df7bfe85e7a79a0d0a76bf646a5a16e44102304
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-06 14:59:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+
+class Neighborhood {
+  constructor(name) {
+    this.id = ++neighborhoodId;
+    this.name = name;
+    
+    store.neighborhoods.push(this);
+  }
+}
