a3e5ff61e4665d440f0bf01c3ed3c49d6bbedcd5
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-06 15:07:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ class Neighborhood {
   
   deliveries() {
     return store.deliveries.filter(delivery => {
-      return delivery.neighborhoodId === this.id
-    })
+      return delivery.neighborhoodId === this.id;
+    });
   }
 }
 
