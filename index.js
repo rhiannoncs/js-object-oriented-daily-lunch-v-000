5fc991eba3b06a6df6122960752adebcd70faadc
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-06 15:21:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ class Neighborhood {
     const meals = [];
     
     deliveries.forEach(function(delivery) {
-      if !(meals.includes())
+      if !(meals.includes(delivery.meal()))
     })
   }
 }
